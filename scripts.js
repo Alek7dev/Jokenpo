245c77f2e7cf448758dd7b0a3c85be5b17171b1d
Alek7dev <alebjj7@gmail.com>
2025-04-30 13:18:39 -0300

    Implementa enum para opções.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,12 @@ const machineScore = document.querySelector('#machineScore')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+
+}
 
 
 const playHuman = (humanChoice) => {
@@ -15,7 +21,7 @@ const playHuman = (humanChoice) => {
 
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -30,9 +36,9 @@ const playTheGame = (human, machine) => {
 
     } else if (
 
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')
+        (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
 
     ) {
         humanScoreNumber++
